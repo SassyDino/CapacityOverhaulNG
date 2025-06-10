a5e2f774808757aa796c4be29f4ce4986b2b8405
SassyDino <sass.dino@pm.me>
2025-06-11 00:14:21 +0100

    Moved event handler to Events source file

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,37 +15,13 @@ void SetupLog()
     spdlog::flush_on(spdlog::level::trace);
 }
 
-struct EventProcessor : public RE::BSTEventSink<RE::TESContainerChangedEvent> {
-    RE::BSEventNotifyControl ProcessEvent(const RE::TESContainerChangedEvent*event, RE::BSTEventSource<RE::TESContainerChangedEvent>*) {
-        
-        auto item = RE::TESForm::LookupByID(event->baseObj)->GetName();
-        std::string from{};
-        std::string to{};
-        if (event->oldContainer) from = RE::TESForm::LookupByID<RE::TESObjectREFR>(event->oldContainer)->GetName();
-        if (event->newContainer) to = RE::TESForm::LookupByID<RE::TESObjectREFR>(event->newContainer)->GetName();
-        
-        if (Settings::bLogContainerEvents && !Settings::bLogPlayerContainerEvents){
-            logger::debug("{}x '{}' from '{}' <0x{:x}> ---> '{}' <0x{:x}>", event->itemCount, item, from, event->oldContainer, to, event->newContainer);
-        } else if (Settings::bLogContainerEvents && Settings::bLogPlayerContainerEvents) {
-            if (event->oldContainer || event->newContainer == 0x14) {
-                logger::debug("{}x '{}' from '{}' <0x{:x}> ---> '{}' <0x{:x}>", event->itemCount, item, from, event->oldContainer, to, event->newContainer);
-            }
-        }
-        
-        return RE::BSEventNotifyControl::kContinue;
-    }
-};
-
 SKSEPluginLoad(const SKSE::LoadInterface *skse) {
     SKSE::Init(skse);
 
     SetupLog();
 
-    auto* eventSink = new EventProcessor();
-    auto* eventSourceHolder = RE::ScriptEventSourceHolder::GetSingleton();
-    eventSourceHolder->AddEventSink<RE::TESContainerChangedEvent>(eventSink);
-
     Papyrus::Register();
+    Events::SinkEventHandlers();
 
     return true;
 }
