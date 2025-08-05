d762af96c672bb0bb77765f5b84b59554b93a149
SassyDino <sass.dino@pm.me>
2025-08-05 04:48:29 +0100

    Refactored event processors for better clarity and organisation

--- include/Events.h
+++ include/Events.h
@@ -4,14 +4,26 @@ namespace Events
 {
     using Result = RE::BSEventNotifyControl;
 
-    class ContainerEventHandler : public RE::BSTEventSink<RE::TESContainerChangedEvent> {
+    class CapacityEventHandler : public RE::BSTEventSink<RE::TESContainerChangedEvent>, public RE::BSTEventSink<RE::TESEquipEvent> {
+        CapacityEventHandler() = default;
+        ~CapacityEventHandler() = default;
+        CapacityEventHandler(const CapacityEventHandler&) = delete;
+        CapacityEventHandler(CapacityEventHandler&&) = delete;
+        CapacityEventHandler& operator=(const CapacityEventHandler&) = delete;
+        CapacityEventHandler& operator=(CapacityEventHandler&&) = delete;
+
         public:
+			// Trigger capacity system methods (adjust count for relevant item category) upon player-centric ContainerChangedEvents (e.g. picking up or dropping an item).
             virtual Result ProcessEvent(const RE::TESContainerChangedEvent*event, RE::BSTEventSource<RE::TESContainerChangedEvent>*source) override;
+			// Trigger capacity system methods (adjust count for relevant item category) upon player-centric EquipEvents.
+            virtual Result ProcessEvent(const RE::TESEquipEvent*event, RE::BSTEventSource<RE::TESEquipEvent>*source) override;
+
+            static CapacityEventHandler& GetSingleton();
 
             static void Register();
     };
 
-    class WeightEventHandler : public RE::BSTEventSink<RE::MenuOpenCloseEvent>, public RE::BSTEventSink<RE::TESEquipEvent>, public RE::BSTEventSink<RE::TESLoadGameEvent> {
+    class WeightEventHandler : public RE::BSTEventSink<RE::MenuOpenCloseEvent>, public RE::BSTEventSink<RE::TESEquipEvent> {
         WeightEventHandler() = default;
         ~WeightEventHandler() = default;
         WeightEventHandler(const WeightEventHandler&) = delete;
@@ -20,11 +32,28 @@ namespace Events
         WeightEventHandler& operator=(WeightEventHandler&&) = delete;
 
         public:
+			// Trigger weight system methods (recalculate carry capacity) upon certain MenuOpenCloseEvents.
             virtual Result ProcessEvent(const RE::MenuOpenCloseEvent*event, RE::BSTEventSource<RE::MenuOpenCloseEvent>*source) override;
+			// Trigger weight system methods (recalculate carry capacity) upon player-centric EquipEvents.
             virtual Result ProcessEvent(const RE::TESEquipEvent*event, RE::BSTEventSource<RE::TESEquipEvent>*source) override;
-            virtual Result ProcessEvent(const RE::TESLoadGameEvent*event, RE::BSTEventSource<RE::TESLoadGameEvent>*source) override;
 
             static WeightEventHandler& GetSingleton();
+
+            static void Register();
+    };
+
+    class LoadGameEventHandler : public RE::BSTEventSink<RE::TESLoadGameEvent> {
+        LoadGameEventHandler() = default;
+        ~LoadGameEventHandler() = default;
+        LoadGameEventHandler(const LoadGameEventHandler&) = delete;
+        LoadGameEventHandler(LoadGameEventHandler&&) = delete;
+        LoadGameEventHandler& operator=(const LoadGameEventHandler&) = delete;
+        LoadGameEventHandler& operator=(LoadGameEventHandler&&) = delete;
+
+        public:
+            virtual Result ProcessEvent(const RE::TESLoadGameEvent*event, RE::BSTEventSource<RE::TESLoadGameEvent>*source) override;
+
+            static LoadGameEventHandler& GetSingleton();
 
             static void Register();
     };
--- src/Events.cpp
+++ src/Events.cpp
@@ -1,17 +1,16 @@
 #include "Events.h"
 #include "WeightHandler.h"
+#include "CapacityHandler.h"
 
 namespace Events
 {
-    auto ContainerEventHandler::ProcessEvent(const RE::TESContainerChangedEvent*event, RE::BSTEventSource<RE::TESContainerChangedEvent>*) -> Result {
+    auto CapacityEventHandler::ProcessEvent(const RE::TESContainerChangedEvent*event, RE::BSTEventSource<RE::TESContainerChangedEvent>*) -> Result {
         auto item = RE::TESForm::LookupByID(event->baseObj)->GetName();
         std::string from{}, to{};
         bool playerContainerEvent = (event->oldContainer || event->newContainer == 0x14);
 
         if (event->oldContainer) from = RE::TESForm::LookupByID<RE::TESObjectREFR>(event->oldContainer)->GetName();
         if (event->newContainer) to = RE::TESForm::LookupByID<RE::TESObjectREFR>(event->newContainer)->GetName();
-
-        
         if (Settings::globalContainerLog) {
             logger::debug("{}x '{}' from '{}' <0x{:x}> ---> '{}' <0x{:x}>", 
                 event->itemCount, item, from, event->oldContainer, to, event->newContainer);
@@ -20,8 +19,27 @@ namespace Events
                 event->itemCount, item, from, event->oldContainer, to, event->newContainer);
         }
 
+        if (playerContainerEvent) {
+            CapacityHandler::Player::AdjustSingleCategory(event);
+            if (CapacityHandler::Player::IsOverCapacity()) {
+                //TODO: Debuff manager/function
+            }
+        }
+
         return Result::kContinue;
     }
+
+    auto CapacityEventHandler::ProcessEvent(const RE::TESEquipEvent*event, RE::BSTEventSource<RE::TESEquipEvent>*) -> Result {
+        // if player equip event:
+        //      recalculate adjusted capacity limits (to account for backpacks, satchels, etc.)
+		//		modify relevant category count (will need an overload of Player::AdjustSingleCategory for TESEquipEvents)
+        //      	recount all item categories (include in above method)
+        //      check if any categories over capacity
+		return Result::kContinue;
+    }
+
+    // Try and add SkillIncrease event to recaclulate capacity limits
+    // And LevelIncrease event to recalculate weight limit
 
     auto WeightEventHandler::ProcessEvent(const RE::MenuOpenCloseEvent*event, RE::BSTEventSource<RE::MenuOpenCloseEvent>*) -> Result {
         bool relevantMenuEvent = (event->menuName == "Cursor Menu");
@@ -39,17 +57,9 @@ namespace Events
         // Currently only looking out for cursor events, as I think that covers most/all player menus, something else might be better though
         if (relevantMenuEvent) {
             logger::debug("{} identified", event->menuName);
-            WeightHandler::UpdateWeightLimit(std::format("[MenuOpenCloseEvent:{}]", menuType));
+            WeightHandler::UpdateWeightLimit(std::format("MenuOpenCloseEvent:{}", menuType));
         }
 
-        return Result::kContinue;
-    }
-
-    auto WeightEventHandler::ProcessEvent(const RE::TESLoadGameEvent*, RE::BSTEventSource<RE::TESLoadGameEvent>*) -> Result {
-        if (Settings::bLogLoadEvents) logger::debug("LoadGameEvent detected by <Events::WeightEventHandler>");
-        
-        // LoadGameEvent might potentially be redundant, might be able to just include "Fader Menu" or "Loading Menu" in the menu event processor
-        WeightHandler::UpdateWeightLimit("[LoadGameEvent]");
         return Result::kContinue;
     }
 
@@ -69,15 +79,30 @@ namespace Events
                 actorName, event->baseObject, event->originalRefr, event->uniqueID, event->equipped);
         }
         
-        if (playerEquipEvent) WeightHandler::UpdateWeightLimit(std::format("[EquipEvent:{}:{}]", actorName, itemName));
+        if (playerEquipEvent) WeightHandler::UpdateWeightLimit(std::format("EquipEvent:{}:{}", actorName, itemName));
 
         return Result::kContinue;
     }
 
-    void ContainerEventHandler::Register() {
-        auto* eventSink = new ContainerEventHandler();
-        auto* eventSourceHolder = RE::ScriptEventSourceHolder::GetSingleton();
-        eventSourceHolder->AddEventSink<RE::TESContainerChangedEvent>(eventSink);
+    auto LoadGameEventHandler::ProcessEvent(const RE::TESLoadGameEvent*, RE::BSTEventSource<RE::TESLoadGameEvent>*) -> Result {
+        if (Settings::bLogLoadEvents) logger::debug("LoadGameEvent detected by <Events::LoadGameEventHandler>");
+        
+        Settings::Init();
+        WeightHandler::UpdateWeightLimit("LoadGameEvent");
+        CapacityHandler::Limits::UpdateBaseValues;
+
+        return Result::kContinue;
+    }
+
+    CapacityEventHandler& CapacityEventHandler::GetSingleton() {
+        static CapacityEventHandler singleton;
+        return singleton;
+    }
+
+    void CapacityEventHandler::Register() {
+        auto& capacityEventHandler = CapacityEventHandler::GetSingleton();
+        RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESContainerChangedEvent>(&capacityEventHandler);
+        RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESEquipEvent>(&capacityEventHandler);
     }
 
     WeightEventHandler& WeightEventHandler::GetSingleton() {
@@ -88,13 +113,23 @@ namespace Events
     void WeightEventHandler::Register() {
         auto& weightEventHandler = WeightEventHandler::GetSingleton();
         RE::UI::GetSingleton()->AddEventSink<RE::MenuOpenCloseEvent>(&weightEventHandler);
-        RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESLoadGameEvent>(&weightEventHandler);
         RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESEquipEvent>(&weightEventHandler);
     }
 
+    LoadGameEventHandler& LoadGameEventHandler::GetSingleton() {
+        static LoadGameEventHandler singleton;
+        return singleton;
+    }
+
+    void LoadGameEventHandler::Register() {
+        auto& loadGameEventHandler = LoadGameEventHandler::GetSingleton();
+        RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESLoadGameEvent>(&loadGameEventHandler);
+    }
+
     void SinkEventHandlers() {
-        ContainerEventHandler::Register();
+        CapacityEventHandler::Register();
         WeightEventHandler::Register();
+        LoadGameEventHandler::Register();
         logger::info("Registered all event handlers.");
     }
 
