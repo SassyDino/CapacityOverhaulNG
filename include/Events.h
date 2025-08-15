e0dc3d1d84c7765159c073deb4f98309e8170967
SassyDino <sass.dino@pm.me>
2025-08-15 23:49:44 +0100

    Event handlers mostly implemented, minus a few remaining hiccups

--- include/Events.h
+++ include/Events.h
@@ -4,7 +4,8 @@ namespace Events
 {
     using Result = RE::BSEventNotifyControl;
 
-    class CapacityEventHandler : public RE::BSTEventSink<RE::TESContainerChangedEvent>, public RE::BSTEventSink<RE::TESEquipEvent> {
+    class CapacityEventHandler : public RE::BSTEventSink<RE::TESContainerChangedEvent>, public RE::BSTEventSink<RE::TESEquipEvent>, 
+		public RE::BSTEventSink<RE::SkillIncrease::Event>, public RE::BSTEventSink<RE::TESMagicEffectApplyEvent> {
         CapacityEventHandler() = default;
         ~CapacityEventHandler() = default;
         CapacityEventHandler(const CapacityEventHandler&) = delete;
@@ -17,13 +18,18 @@ namespace Events
             virtual Result ProcessEvent(const RE::TESContainerChangedEvent*event, RE::BSTEventSource<RE::TESContainerChangedEvent>*source) override;
 			// Trigger capacity system methods (adjust count for relevant item category) upon player-centric EquipEvents.
             virtual Result ProcessEvent(const RE::TESEquipEvent*event, RE::BSTEventSource<RE::TESEquipEvent>*source) override;
+			// Trigger capacity system methods (adjust count for relevant item category) upon player Skill increase.
+			virtual Result ProcessEvent(const RE::SkillIncrease::Event*event, RE::BSTEventSource<RE::SkillIncrease::Event>*source) override;
+
+			virtual Result ProcessEvent(const RE::TESMagicEffectApplyEvent*event, RE::BSTEventSource<RE::TESMagicEffectApplyEvent>*source) override;
 
             static CapacityEventHandler& GetSingleton();
 
             static void Register();
     };
 
-    class WeightEventHandler : public RE::BSTEventSink<RE::MenuOpenCloseEvent>, public RE::BSTEventSink<RE::TESEquipEvent> {
+    class WeightEventHandler : public RE::BSTEventSink<RE::MenuOpenCloseEvent>, public RE::BSTEventSink<RE::TESEquipEvent>, 
+		public RE::BSTEventSink<RE::LevelIncrease::Event>, public RE::BSTEventSink<RE::TESMagicEffectApplyEvent> {
         WeightEventHandler() = default;
         ~WeightEventHandler() = default;
         WeightEventHandler(const WeightEventHandler&) = delete;
@@ -36,6 +42,10 @@ namespace Events
             virtual Result ProcessEvent(const RE::MenuOpenCloseEvent*event, RE::BSTEventSource<RE::MenuOpenCloseEvent>*source) override;
 			// Trigger weight system methods (recalculate carry capacity) upon player-centric EquipEvents.
             virtual Result ProcessEvent(const RE::TESEquipEvent*event, RE::BSTEventSource<RE::TESEquipEvent>*source) override;
+			// Trigger capacity system methods (adjust count for relevant item category) upon player Level increase.
+			virtual Result ProcessEvent(const RE::LevelIncrease::Event*event, RE::BSTEventSource<RE::LevelIncrease::Event>*source) override;
+
+			virtual Result ProcessEvent(const RE::TESMagicEffectApplyEvent*event, RE::BSTEventSource<RE::TESMagicEffectApplyEvent>*source) override;
 
             static WeightEventHandler& GetSingleton();
 
--- src/Events.cpp
+++ src/Events.cpp
@@ -1,95 +1,172 @@
 #include "Events.h"
 #include "WeightHandler.h"
 #include "CapacityHandler.h"
+#include "BuffsDebuffs.h"
 
 namespace Events
 {
     auto CapacityEventHandler::ProcessEvent(const RE::TESContainerChangedEvent*event, RE::BSTEventSource<RE::TESContainerChangedEvent>*) -> Result {
-        auto item = RE::TESForm::LookupByID(event->baseObj)->GetName();
-        std::string from{}, to{};
-        bool playerContainerEvent = (event->oldContainer || event->newContainer == 0x14);
+        bool playerEvent = (event->oldContainer == 0x14 || event->newContainer == 0x14);
+        if (!playerEvent) { return Result::kContinue; }
 
-        if (event->oldContainer) from = RE::TESForm::LookupByID<RE::TESObjectREFR>(event->oldContainer)->GetName();
-        if (event->newContainer) to = RE::TESForm::LookupByID<RE::TESObjectREFR>(event->newContainer)->GetName();
-        if (Settings::globalContainerLog) {
-            logger::debug("{}x '{}' from '{}' <0x{:x}> ---> '{}' <0x{:x}>", 
-                event->itemCount, item, from, event->oldContainer, to, event->newContainer);
-        } else if (Settings::playerContainerLogOnly && playerContainerEvent) {
-            logger::debug("{}x '{}' from '{}' <0x{:x}> ---> '{}' <0x{:x}>", 
-                event->itemCount, item, from, event->oldContainer, to, event->newContainer);
-        }
+		auto item = RE::TESForm::LookupByID(event->baseObj)->GetName();
+		std::string from{}, to{};
+		if (event->oldContainer) from = RE::TESForm::LookupByID<RE::TESObjectREFR>(event->oldContainer)->GetName();
+		if (event->newContainer) to = RE::TESForm::LookupByID<RE::TESObjectREFR>(event->newContainer)->GetName();
 
-        if (playerContainerEvent) {
-            CapacityHandler::Player::AdjustSingleCategory(event);
-            if (CapacityHandler::Player::IsOverCapacity()) {
-                //TODO: Debuff manager/function
-            }
-        }
+		//RE::TESObjectREFR* itemRef = nullptr;
+		auto refHandle = event->reference;
+		if (refHandle) {
+			logger::debug("refHandle found...");
+			//RE::TESForm* refForm = RE::TESForm::LookupByID(refHandle.native_handle());
+			//if (refForm) {
+			//	logger::debug("refForm found...");
+			//	itemRef = refForm->AsReference();
+			//}
+		}
+
+		// Consuming or dropping items triggers ContainerChangedEvents with a newContainer of 0x0, but dropping items provides a refHandle we can use to filter out consumption triggers
+		if ((event->newContainer > 0x0) || refHandle) {
+			logger::debug("<CapacityEventHandler::ContainerChanged> -> {}x '{}' from '{}' <0x{:x}> ---> '{}' <0x{:x}> | uID = {}", 
+				event->itemCount, item, from, event->oldContainer, to, event->newContainer, event->uniqueID);
+
+			CapacityHandler::Player::AdjustSingleCategory(event);
+			Debuffs::CapacityEffects();
+		}
+        
 
         return Result::kContinue;
     }
 
     auto CapacityEventHandler::ProcessEvent(const RE::TESEquipEvent*event, RE::BSTEventSource<RE::TESEquipEvent>*) -> Result {
-        // if player equip event:
-        //      recalculate adjusted capacity limits (to account for backpacks, satchels, etc.)
-		//		modify relevant category count (will need an overload of Player::AdjustSingleCategory for TESEquipEvents)
-        //      	recount all item categories (include in above method)
-        //      check if any categories over capacity
+		auto equipActor = event->actor.get();
+		auto item = RE::TESForm::LookupByID(event->baseObject);
+
+		bool playerEvent = equipActor->GetFormID() == 0x14;
+		bool validType = item->Is(RE::FormType::Armor) || item->Is(RE::FormType::Weapon) || item->Is(RE::FormType::AlchemyItem) || item->Is(RE::FormType::Ingredient);
+		if (!(playerEvent && validType)) { return Result::kContinue; }
+
+		logger::debug("<CapacityEventHandler::Equip> -> BaseObj: '{} [0x{:x}]', Equipped: '{}'", item->GetName(), item->GetFormID(), event->equipped);
+
+		// What to do if the item being equipped is expected to increase the player's item capacity
+		//TODO: Complete this once backpack system is implemented
+		bool storageItem = false;
+		if (storageItem) {
+			CapacityHandler::Limits::CalculatePlayValues();
+		}
+
+		// Prevent equipped items from taking up storage space
+		auto itemCategory = CapacityHandler::Player::GetEquipCategory(item);
+		if (event->equipped) {
+			CapacityHandler::Player::DecreaseCategory(itemCategory, 1);
+		} else {
+			CapacityHandler::Player::IncreaseCategory(itemCategory, 1);
+		}
+		
+		CapacityHandler::Limits::CalculatePlayValues();
+		CapacityHandler::Player::UpdateTotalCount();
+		CapacityHandler::Player::AllCategoriesLog();
+		Debuffs::CapacityEffects();
+
 		return Result::kContinue;
     }
 
-    // Try and add SkillIncrease event to recaclulate capacity limits
-    // And LevelIncrease event to recalculate weight limit
+	auto CapacityEventHandler::ProcessEvent(const RE::SkillIncrease::Event*event, RE::BSTEventSource<RE::SkillIncrease::Event>*) -> Result {
+		logger::debug("<CapacityEventHandler::SkillIncrease> -> Skill: '{}'", event->actorValue);
+
+		CapacityHandler::Limits::CalculatePlayValues();
+		CapacityHandler::Player::AllCategoriesLog();
+
+		return Result::kContinue;
+	}
+
+	auto CapacityEventHandler::ProcessEvent(const RE::TESMagicEffectApplyEvent*event, RE::BSTEventSource<RE::TESMagicEffectApplyEvent>*) -> Result {
+		auto playerEvent = (event->caster->GetFormID() == 0x14) || (event->target->GetFormID() == 0x14);
+		if (!playerEvent) { return Result::kContinue; }
+
+		auto MGEFID = event->magicEffect;
+		auto MGEF = RE::TESForm::LookupByID<RE::EffectSetting>(MGEFID);
+		auto MGEFAV = MGEF->data.primaryAV;
+		bool isValid = (MGEFAV == RE::ActorValue::kAlchemy) || (MGEFAV == RE::ActorValue::kArchery) || (MGEFAV == RE::ActorValue::kSpeech) || (MGEFAV == RE::ActorValue::kLockpicking) || (MGEFAV == RE::ActorValue::kPickpocket);
+		
+		if (MGEF && isValid) {
+			logger::debug("<CapacityEventHandler::MagicEffect> -> Caster: '{}', Target: '{}', MGEF: '{} 0x[{:X}]'", 
+				event->caster->GetName(), event->target->GetName(), RE::TESForm::LookupByID(event->magicEffect)->GetName(), event->magicEffect);
+
+			CapacityHandler::Limits::CalculatePlayValues();
+			CapacityHandler::Player::AllCategoriesLog();
+		}
+
+		return Result::kContinue;
+	}
 
     auto WeightEventHandler::ProcessEvent(const RE::MenuOpenCloseEvent*event, RE::BSTEventSource<RE::MenuOpenCloseEvent>*) -> Result {
-        bool relevantMenuEvent = (event->menuName == "Cursor Menu");
-        auto menuType = event->menuName.c_str();
+		//NOTE: If I get some sort of Magic Effect event working, I might not even need Menu events
+		//? If not though, I could consider: InventoryMenu, MagicMenu, ContainerMenu, StatsMenu
+        bool relevantMenuEvent = ((event->menuName == "InventoryMenu") || (event->menuName == "StatsMenu"));
+        if (!relevantMenuEvent) {return Result::kContinue;}
+		
+		auto menuType = event->menuName.c_str();
         std::string str(menuType);
 
-        if (Settings::globalMenuLog) {
-            logger::debug("MenuOpenCloseEvent detected by <Events::WeightEventHandler> -> Menu name: '{}', Opening: '{}'", 
-                menuType, event->opening);
-        } else if (Settings::relevantMenuLogOnly && relevantMenuEvent) {
-            logger::debug("MenuOpenCloseEvent detected by <Events::WeightEventHandler> -> Menu name: '{}', Opening: '{}'", 
-                menuType, event->opening);
-        }
-
-        // Currently only looking out for cursor events, as I think that covers most/all player menus, something else might be better though
-        if (relevantMenuEvent) {
-            logger::debug("{} identified", event->menuName);
-            WeightHandler::UpdateWeightLimit(std::format("MenuOpenCloseEvent:{}", menuType));
-        }
+		logger::debug("<WeightEventHandler::MenuOpenClose> -> Menu name: '{}', Opening: '{}'", menuType, event->opening);
+		WeightHandler::UpdateWeightLimit();
 
         return Result::kContinue;
     }
 
     auto WeightEventHandler::ProcessEvent(const RE::TESEquipEvent*event, RE::BSTEventSource<RE::TESEquipEvent>*) -> Result {
-        RE::NiPointer<RE::TESObjectREFR> equipActor = event->actor;
-        std::string actorName = equipActor->GetName();
-        std::string itemName = RE::TESForm::LookupByID<RE::TESObjectREFR>(event->baseObject)->GetName();
+        auto equipActor = event->actor.get();
+		auto item = RE::TESForm::LookupByID(event->baseObject);
+
+		bool playerEvent = equipActor->GetFormID() == 0x14;
+		bool invalidItem = item->Is(RE::FormType::Spell) || item->Is(RE::FormType::AlchemyItem) || item->Is(RE::FormType::Ingredient) || item->Is(RE::FormType::Book) || item->Is(RE::FormType::Ammo);
         
-        RE::FormID actorID = equipActor->formID;
-        bool playerEquipEvent = (actorID == 0x14);
-        
-        if (Settings::globalEquipLog) {
-            logger::debug("EquipEvent detected by <Events::WeightEventHandler> -> Actor: '{}', BaseObj: '0x{:x}', originalRef: '0x{:x}', uniqueID: '{}', Equipped: '{}'", 
-                actorName, event->baseObject, event->originalRefr, event->uniqueID, event->equipped);
-        } else if (Settings::playerEquipLogOnly && playerEquipEvent) {
-            logger::debug("EquipEvent detected by <Events::WeightEventHandler> -> Actor: '{}', BaseObj: '0x{:x}', originalRef: '0x{:x}', uniqueID: '{}', Equipped: '{}'", 
-                actorName, event->baseObject, event->originalRefr, event->uniqueID, event->equipped);
+        if ((playerEvent) && !(invalidItem)) {
+			std::string actorName = equipActor->GetName();
+			std::string itemName = item->GetName();
+
+            logger::debug("<WeightEventHandler::Equip> -> Actor: '{}', BaseObj: '{} [0x{:X}]', Equipped: '{}', oRef: '0x{:x}', uID: '{}'", 
+                actorName, itemName, event->baseObject, event->equipped, event->originalRefr, event->uniqueID);
+			
+			WeightHandler::UpdateWeightLimit();
+			Utils::LogStaminaAVs();
         }
-        
-        if (playerEquipEvent) WeightHandler::UpdateWeightLimit(std::format("EquipEvent:{}:{}", actorName, itemName));
 
         return Result::kContinue;
     }
 
+	auto WeightEventHandler::ProcessEvent(const RE::LevelIncrease::Event*event, RE::BSTEventSource<RE::LevelIncrease::Event>*) -> Result {
+		logger::debug("<WeightEventHandler::LevelIncrease> -> Level: {}", event->newLevel);
+
+		WeightHandler::UpdateWeightLimit();
+
+		return Result::kContinue;
+	}
+
+	auto WeightEventHandler::ProcessEvent(const RE::TESMagicEffectApplyEvent*event, RE::BSTEventSource<RE::TESMagicEffectApplyEvent>*) -> Result {
+		auto playerEvent = (event->caster->GetFormID() == 0x14) || (event->target->GetFormID() == 0x14);
+		if (!playerEvent) { return Result::kContinue; }
+
+		auto MGEFID = event->magicEffect;
+		auto MGEF = RE::TESForm::LookupByID<RE::EffectSetting>(MGEFID);
+		bool isConc = MGEF->data.castingType == RE::MagicSystem::CastingType::kConcentration;
+		bool isGenericDamage = MGEFID == 0xEA075;
+		
+		if (MGEF && !isConc && !isGenericDamage) {
+			logger::debug("<WeightEventHandler::MagicEffect> -> Caster: '{}', Target: '{}', MGEF: '{} 0x[{:X}]'", 
+				event->caster->GetName(), event->target->GetName(), RE::TESForm::LookupByID(event->magicEffect)->GetName(), event->magicEffect);
+
+			WeightHandler::UpdateWeightLimit();
+		}
+
+		return Result::kContinue;
+	}
+
     auto LoadGameEventHandler::ProcessEvent(const RE::TESLoadGameEvent*, RE::BSTEventSource<RE::TESLoadGameEvent>*) -> Result {
-        if (Settings::bLogLoadEvents) logger::debug("LoadGameEvent detected by <Events::LoadGameEventHandler>");
+        logger::debug("<LoadGameEventHandler::LoadGame>");
         
         Settings::Init();
-        WeightHandler::UpdateWeightLimit("LoadGameEvent");
-        CapacityHandler::Limits::UpdateBaseValues;
 
         return Result::kContinue;
     }
@@ -103,6 +180,8 @@ namespace Events
         auto& capacityEventHandler = CapacityEventHandler::GetSingleton();
         RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESContainerChangedEvent>(&capacityEventHandler);
         RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESEquipEvent>(&capacityEventHandler);
+		RE::SkillIncrease::GetEventSource()->AddEventSink<RE::SkillIncrease::Event>(&capacityEventHandler);
+		RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESMagicEffectApplyEvent>(&capacityEventHandler);
     }
 
     WeightEventHandler& WeightEventHandler::GetSingleton() {
@@ -114,6 +193,8 @@ namespace Events
         auto& weightEventHandler = WeightEventHandler::GetSingleton();
         RE::UI::GetSingleton()->AddEventSink<RE::MenuOpenCloseEvent>(&weightEventHandler);
         RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESEquipEvent>(&weightEventHandler);
+		RE::LevelIncrease::GetEventSource()->AddEventSink<RE::LevelIncrease::Event>(&weightEventHandler);
+		RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESMagicEffectApplyEvent>(&weightEventHandler);
     }
 
     LoadGameEventHandler& LoadGameEventHandler::GetSingleton() {
